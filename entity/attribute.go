477c23fc98d015c71356a2bcbb3573e41b2cb96a
zouzhihao-994 <zouzhihao994@gmail.com>
2021-03-28 18:13:49 +0800

    basic func of node & attribute

--- entity/attribute.go
+++ entity/attribute.go
@@ -1,5 +1,17 @@
 package entity
 
+import (
+	"github.com/zouzhihao-994/spider-go/exception"
+)
+
 type Attribute struct {
 	body map[string]string
+}
+
+func (this *Attribute) update(k, v string) (bool, error) {
+	if this.body[k] == "" {
+		return false, exception.RuntimeException(exception.AttributeNotExistException)
+	}
+	this.body[k] = v
+	return true, nil
 }
--- entity/node.go
+++ entity/node.go
@@ -1,8 +1,29 @@
 package entity
 
+import "github.com/zouzhihao-994/spider-go/exception"
+
 type node struct {
 	name         string
 	attributes   *Attribute
 	associated   map[string]*Relation
 	beAssociated map[string]*Relation
+}
+
+func Node(name string, attributes *Attribute) (*node, error) {
+	if name == "" {
+		return nil, exception.RuntimeException("name is blank")
+	}
+	if attributes == nil {
+		return nil, exception.RuntimeException("attributes is nil")
+	}
+	return &node{
+		name:         name,
+		attributes:   attributes,
+		associated:   make(map[string]*Relation),
+		beAssociated: make(map[string]*Relation),
+	}, nil
+}
+
+func (this *node) updateAttribute(k, v string) (bool, error) {
+	return this.attributes.update(k, v)
 }
