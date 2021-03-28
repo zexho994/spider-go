bfba9e0aafabeab65999cec95bee838117111068
zouzhihao-994 <zouzhihao994@gmail.com>
2021-03-28 22:38:32 +0800

    update attribute

--- entity/attribute.go
+++ entity/attribute.go
@@ -8,9 +8,15 @@ type Attribute struct {
 	body map[string]string
 }
 
+func NewAttribute(attr map[string]string) *Attribute {
+	return &Attribute{
+		body: attr,
+	}
+}
+
 func (this *Attribute) update(k, v string) (bool, error) {
 	if this.body[k] == "" {
-		return false, exception.RuntimeException(exception.AttributeNotExistException)
+		return false, exception.RuntimeException(exception.AttributeNotExistError)
 	}
 	this.body[k] = v
 	return true, nil
