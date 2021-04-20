92107c7d39163008739a7725458b0dff67e52eca
zexho <zouzhihao994@gmail.com>
2021-04-20 18:56:19 +0800

    refactor skipList

--- structs/skiplist.go
+++ structs/skiplist.go
@@ -1,41 +1,48 @@
 package structs
 
-type LEVEL uint8
-type LENGTH uint16
-type SCORE float64
 type VALUE string
 
 type skipListMap map[string]*SkipList
 
 type SkipList struct {
-	level  LEVEL
-	length LENGTH
-	header *SkipListNode
-	tail   *SkipListNode
+	level        uint8
+	length       uint16
+	count        int64
+	header, tail *SkipListNode
 }
 
 type SkipListNode struct {
-	val   VALUE
-	score SCORE
-	level []struct {
-		forward *SkipListNode
-		span    LENGTH
-	}
-	bucket   []*SkipListNode
+	val      VALUE
+	score    float64
+	level    []*SkipListLevel
 	backward *SkipListNode
 }
 
+type SkipListLevel struct {
+	forward *SkipListNode
+	span    uint8
+}
+
+func initSkipList() *SkipList {
+	return &SkipList{
+		level:  0,
+		length: 0,
+		count:  0,
+		header: &SkipListNode{level: make([]*SkipListLevel, 64)},
+	}
+}
+
 // add or update the key&val to SkipList
-func (skipListMap) add(key string, val VALUE, score SCORE) {
+func (SkipList) add(key string, val VALUE, score float64) {
 
 }
 
 // remove a val from SkipList
-func (skipListMap) remove(key string, val VALUE) {
+func (SkipList) remove(key string, val VALUE) {
 
 }
 
 // range find SkipListNode by score
-func (skipListMap) rangeByScore(key string, s1, s2 SCORE) {
+func (SkipList) rangeByScore(key string, s1, s2 float64) {
 
 }
